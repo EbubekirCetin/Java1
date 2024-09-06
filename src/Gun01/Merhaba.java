741b0ccab0bcc66f93bfcd709976912962f0732f
Pc <bekir9090@gmail.com>
2024-09-06 22:29:48 +0300

    wish List Testi eklendi

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -6,13 +6,19 @@ public class Merhaba {
 
         System.out.println("Merhaba 1");
 
-        System.out.println("Merhaba 2");
+
 
         System.out.println("Task 1 kodları ");
 
         System.out.println("Task 2 kodları");
 
         System.out.println("Task 3 kodları");
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println("Task 4 yapıldı");
+
+        }
 
 
 
